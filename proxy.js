b4ce3876f477446b4fbeca723761a2e1b2ddd258
Krispin Schulz <krispinone@googlemail.com>
2017-01-30 19:13:42 +0100

    fix proxy

--- proxy.js
+++ proxy.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 module.exports = (config) => {
-  const { port } = config;
+  const { port, host } = config;
   const server = http.createServer();
   server.start = () => {
     server.listen(port, host);
