441c29dc9ab9257e2496dfedb720073fde1aff40
Krispin Schulz <krispinone@googlemail.com>
2017-01-30 19:01:50 +0100

    fix test

--- test.js
+++ test.js
@@ -1,17 +1,19 @@
 const http = require('http');
 const config = {
-  port: 3001
+  port: 3001,
+  host: 'localhost'
 };
 const proxy = require('./proxy')(config);
 
 proxy.start(() => {
   const req = http.request({
-    hostname: 'localhost',
+    hostname: config.host,
     port: config.port,
     path: '/session',
     method: 'POST'
   }, (res) => {
     res.on('end', () => {
+      console.log('lol');
     });
   });
 });
